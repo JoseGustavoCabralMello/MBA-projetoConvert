1b8f24a67793b95cd3541ff11297ac12ebfbd348
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2024-07-16 06:47:33 -0300

    Obtendo a moede selecionada

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
-// Manipulando o input amount para receber somente números  
+// Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.replace(hasCharactersRegex, "")
 })
+
+// Captando o evento de submit (enviar) do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault()
+}
