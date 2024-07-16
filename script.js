aa507fa474efe407f4c356fdd4f245273427124d
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2024-07-16 08:50:58 -0300

    Formatando o total

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const result = document.getElementById("result")
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g
-  amount.value = amount.replace(hasCharactersRegex, "")
+  amount.value = amount.value.replace(hasCharactersRegex, "")
 })
 
 // Captando o evento de submit (enviar) do formulário.
@@ -40,15 +40,22 @@ function convertCurrency(amount, price, symbol) {
     // Exibindo a cotação da moeda selecionada.
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
 
+    // Calcula o total
+    let total = amount * price
+    
+    // Verifica se o resultado não é um número.
+    if (isNaN(total)) {
+      return alert("Por favor, digite o valor corretamente para converter.")
+    }
+    
+    // Formatar o valor total
+    total = formatCurrencyBRL(total).replace("R$", "")
+    
+    // Exibe o resultado total
+    result.textContent = `${total} Reais`
+
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result")
-
-    // Calcula o resultato
-    let total = amount * price
-
-    // Exibe o resultado total
-    result.textContent = total
-
   } catch (error) {
     // Remove a classe do footer removendo ele de tela.
     footer.classList.remove("show-result")
